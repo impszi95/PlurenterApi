d576a7f2cab3651ea083cd9a47d26a2fa6880c69
Varga Bence <impszi95@gmail.com>
2021-01-16 02:04:17 +0100

    prod ip

--- src/main/java/com/Home/Tinder/Service/NotificationService.java
+++ src/main/java/com/Home/Tinder/Service/NotificationService.java
@@ -23,7 +23,7 @@ public class NotificationService {
     }
 
     public void NewMatchNotification(Notification notification) {
-        String url = "http://localhost:8082";
+        String url = "http://194.156.81.191:8082";
 
         HttpEntity<Notification> request = new HttpEntity<Notification>(notification);
 
