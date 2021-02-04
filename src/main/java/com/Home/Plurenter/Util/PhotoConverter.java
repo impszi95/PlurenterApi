aae95f626a0fb018b740952c08ceb748ffc80c12
Varga Bence <impszi95@gmail.com>
2021-02-04 23:21:30 +0100

    PNG available to upload

--- src/main/java/com/Home/Plurenter/Util/PhotoConverter.java
+++ src/main/java/com/Home/Plurenter/Util/PhotoConverter.java
@@ -1,6 +1,7 @@
 package com.Home.Plurenter.Util;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -10,14 +11,17 @@ public class PhotoConverter {
     public static BufferedImage ConvertBytesToImage(byte[] imageData) {
         ByteArrayInputStream bais = new ByteArrayInputStream(imageData);
         try {
-            return ImageIO.read(bais);
+            BufferedImage img = ImageIO.read(bais);
+            return img;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
     public static byte[] ConvertImageToBytes(BufferedImage img) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(img, "jpg", baos);
+        BufferedImage newImage = new BufferedImage( img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+        newImage.createGraphics().drawImage(img,0,0,Color.WHITE,null);
+        ImageIO.write(newImage, "jpg", baos);
         return baos.toByteArray();
     }
 }
